6c85d019af2b4336d81613205c6128a59ff510b3
Arsenio Benitez <Arseniobenitezjr@hotmail.com>
2020-12-14 22:04:16 -0500

    Add project files.

---
+++ DomainModel/Concrete/FakeProductsRepository.cs
@@ -0,0 +1,24 @@
+﻿using DomainModel.Abstract;
+using DomainModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DomainModel.Concrete
+{
+    class FakeProductsRepository : IProductsRepository
+    {
+        private static IQueryable<Product> fakeProducts = new List<Product>
+        {
+            new Product { Name = "Football", Price = 25},
+            new Product { Name = "Surf Board", Price = 179},
+            new Product { Name = "Boxing Gloves", Price = 35}
+        }.AsQueryable();
+        public IQueryable<Product> Products
+        {
+            get { return fakeProducts;  }
+        }
+
+    }
+}
